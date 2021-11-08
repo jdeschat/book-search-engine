dd474eee24928d34c8362bb00e11d6452e22f12d
Joelle DesChatelets <jdeschat@gmail.com>
2021-11-08 18:59:08 -0500

    troubleshooting

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql
+// } from "@apollo/client";
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache()
+// });
+
+// // ...ApolloClient instantiated here...
+
+// client
+//   .query({
+//     query: gql`
+//       query TestQuery {
+//         launch(id: 56) {
+//           id
+//           mission {
+//             name
+//           }
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
